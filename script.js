88cb8c7164ad8a948654c7e9c0cb38d4f1b17349
Nimbus <hossain.mona006@gmail.com>
2025-04-22 00:39:56 +0530

    added filters

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ const searchBtn=document.querySelector(".search-btn")
 const imageSection=document.querySelector(".image-result")
 const apiKey="2aCPEdnRDeBRuyeLtaRDpbzzJV7dlBGP";
 const showMoreBtn=document.querySelector(".showmore-btn")
-let page =1;
+
+let page =1,purity=100;
 let query;
 
 async function search(keyword) {
     console.log("searching")
     try {
-        const url=`https://wallhaven.cc/api/v1/search?q=${keyword}&page=${page}&apikey=${apiKey}`
+        const url=`https://wallhaven.cc/api/v1/search?q=${keyword}&purity=${purity}&page=${page}&apikey=${apiKey}`
         
         console.log(url)
         // const result = await fetch(`https://wallhaven.cc/api/v1/search?q=${keyword}&apikey=${apiKey}`)
@@ -29,10 +30,13 @@ async function search(keyword) {
 
 searchBtn.addEventListener("click",()=>{
     query=input.value;
+    purityCheck();
     search(query)
     console.log(query)
     imageSection.innerHTML="";
     page=1;
+    
+    console.log(purity)
 })
 
 
@@ -42,7 +46,9 @@ function displayImages(imgObj){
         let container=document.createElement("div")
         let img=document.createElement("img")
         let link=document.createElement("a")
+
         link.href=image.path
+        link.target="_blank"
         container.classList.add("img-container")
         console.log(1);
         // img.crossOrigin = "anonymous"
@@ -60,3 +66,27 @@ showMoreBtn.addEventListener("click",()=>{
     search(query)
 })
 
+function purityCheck(){
+    const sketchyFilter=document.getElementById("sketchy");
+    const NSFWFilter=document.getElementById("NSFW");
+
+    if(sketchyFilter.checked){
+        purity+=10;
+    }
+    if(NSFWFilter.checked){
+        purity+=1;
+    }
+    if(!sketchyFilter.checked && !NSFWFilter.checked){
+        purity=100;
+    }
+}
+
+// input.addEventListener("keydown", function(event) {
+    
+//     if (event.key === "enter") {
+      
+     
+      
+//       document.querySelector(".searchBtn").click();
+//     }
+//   }); 
